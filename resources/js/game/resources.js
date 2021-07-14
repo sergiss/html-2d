81df4f362a8051c8b6472967e18a5fe0d4dddbdf
Sergio S <sergi.ss4@gmail.com>
2021-07-14 12:02:32 +0200

    Update resources.js
    Update

--- resources/js/game/resources.js
+++ resources/js/game/resources.js
@@ -32,52 +32,52 @@ var loadImage = function(src) {
     result.src = src;
     return result;
 }
-var skeletonImg = loadImage("/resources/data/skeleton.png");
-var batImg = loadImage("/resources/data/bat.png");
-var characterImg = loadImage("/resources/data/character.png");
-var platformImg = loadImage("/resources/data/platform.png");
-var spikesImg = loadImage("/resources/data/spikes.png");
-var springImg = loadImage("/resources/data/spring.png")
-var snakeImg = loadImage("/resources/data/snake.png");
-var doorImg = loadImage("/resources/data/platform.png");
-var keyImg = loadImage("/resources/data/key.png");
-var heartImg = loadImage("/resources/data/heart.png");
-var coinImg = loadImage("/resources/data/coin.png");
-var clockImg = loadImage("/resources/data/clock.png");
-var canonImg = loadImage("/resources/data/canon.png");
+var skeletonImg = loadImage("./resources/data/skeleton.png");
+var batImg = loadImage("./resources/data/bat.png");
+var characterImg = loadImage("./resources/data/character.png");
+var platformImg = loadImage("./resources/data/platform.png");
+var spikesImg = loadImage("./resources/data/spikes.png");
+var springImg = loadImage("./resources/data/spring.png")
+var snakeImg = loadImage("./resources/data/snake.png");
+var doorImg = loadImage("./resources/data/platform.png");
+var keyImg = loadImage("./resources/data/key.png");
+var heartImg = loadImage("./resources/data/heart.png");
+var coinImg = loadImage("./resources/data/coin.png");
+var clockImg = loadImage("./resources/data/clock.png");
+var canonImg = loadImage("./resources/data/canon.png");
 
-var parallax1 = loadImage("/resources/data/parallax/layer_01_1920 x 1080.png");
-var parallax2 = loadImage("/resources/data/parallax/layer_02_1920 x 1080.png");
-var parallax3 = loadImage("/resources/data/parallax/layer_03_1920 x 1080.png");
-var parallax4 = loadImage("/resources/data/parallax/layer_04_1920 x 1080.png");
-var parallax5 = loadImage("/resources/data/parallax/layer_05_1920 x 1080.png");
-var parallax6 = loadImage("/resources/data/parallax/layer_06_1920 x 1080.png");
-var parallax7 = loadImage("/resources/data/parallax/layer_07_1920 x 1080.png");
-var parallax8 = loadImage("/resources/data/parallax/layer_08_1920 x 1080.png");
+var parallax1 = loadImage("./resources/data/parallax/layer_01_1920 x 1080.png");
+var parallax2 = loadImage("./resources/data/parallax/layer_02_1920 x 1080.png");
+var parallax3 = loadImage("./resources/data/parallax/layer_03_1920 x 1080.png");
+var parallax4 = loadImage("./resources/data/parallax/layer_04_1920 x 1080.png");
+var parallax5 = loadImage("./resources/data/parallax/layer_05_1920 x 1080.png");
+var parallax6 = loadImage("./resources/data/parallax/layer_06_1920 x 1080.png");
+var parallax7 = loadImage("./resources/data/parallax/layer_07_1920 x 1080.png");
+var parallax8 = loadImage("./resources/data/parallax/layer_08_1920 x 1080.png");
 
 var jumpSound;
 var musicSound;
 var keySound;
 var springSound;
 window.addEventListener("load", function() {
-    jumpSound = new Sound("resources/data/jump.mp3");
+    jumpSound = new Sound("./resources/data/jump.mp3");
     jumpSound.sound.volume = 0.5;
 
-    heartSound = new Sound("resources/data/heart.mp3");
+    heartSound = new Sound("./resources/data/heart.mp3");
     heartSound.sound.volume = 1;
 
-    keySound = new Sound("resources/data/key.mp3");
+    keySound = new Sound("./resources/data/key.mp3");
     keySound.sound.volume = 1;
 
-    coinSound = new Sound("resources/data/coin.mp3");
+    coinSound = new Sound("./resources/data/coin.mp3");
     coinSound.sound.volume = 1;
 
-    explosionSound = new Sound("resources/data/explosion.mp3");
+    explosionSound = new Sound("./resources/data/explosion.mp3");
     explosionSound.sound.volume = 0.5;
 
-    springSound = new Sound("resources/data/spring.mp3");
+    springSound = new Sound("./resources/data/spring.mp3");
     springSound.sound.volume = 0.5;
 
-    musicSound = new Sound("resources/data/music/music3.mp3");
+    musicSound = new Sound("./resources/data/music/music3.mp3");
     musicSound.sound.volume = 0.5;
 });

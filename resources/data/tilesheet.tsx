099d58c21b3fa2360f1f83858a6d9e8ecfd99bd7
Sergio Soriano <sergi.ss4@gmail.com>
2021-11-03 21:42:38 +0100

    Update

--- resources/data/tilesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tilesheet" tilewidth="16" tileheight="16" spacing="4" margin="2" tilecount="64" columns="8">
- <image source="tilesheet.png" width="160" height="160"/>
-</tileset>

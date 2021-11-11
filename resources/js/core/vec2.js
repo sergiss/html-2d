06d835333cda4e815d0172afef372455bec80fae
Sergi S <sergi.ss4@gmail.com>
2021-11-11 13:06:16 +0100

    Update

--- resources/js/core/vec2.js
+++ resources/js/core/vec2.js
@@ -27,7 +27,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
-export class Vec2 {
+class Vec2 {
   constructor(x = 0, y = 0) {
 
     if (x instanceof Vec2) {
